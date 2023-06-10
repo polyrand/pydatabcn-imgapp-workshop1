625ce33ac1e66cd9ba3e44bfbe6c02098c854909
polyrand <37962604+polyrand@users.noreply.github.com>
2023-06-10 10:50:30 +0200

    update

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import sys
+
 import uvicorn
+from fastai.vision.all import *
 from fastapi import FastAPI
+from fastapi import File
 from fastapi import Form
 from fastapi import Request
+from fastapi import UploadFile
 from fastapi.templating import Jinja2Templates
 from typing_extensions import Annotated
 
@@ -10,15 +15,31 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
+def get_x(x):
+    return 1
+
+
+def get_y(x):
+    return 1
+
+
+sys.modules["__main__"].get_x = get_x
+sys.modules["__main__"].get_y = get_y
+
+
+learn = load_learner("test.pkl")
+
+
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html.j2", {"request": request})
 
 
-@app.get("/test")
-async def test(request: Request, name: str):
+@app.post("/test")
+async def test(request: Request, imagen_entrada: Annotated[bytes, File()]):
+    print(imagen_entrada)
     return templates.TemplateResponse(
-        "response.html.j2", {"request": request, "name": name}
+        "response.html.j2", {"request": request, "name": imagen_entrada}
     )
 
 
