5c750bfd4723275b0d550ac777ae4ed353726bf6
polyrand <37962604+polyrand@users.noreply.github.com>
2023-06-10 11:11:37 +0200

    templates/

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import base64
+import json
 import sys
+import uuid
 
 import uvicorn
 from fastai.vision.all import *
@@ -35,12 +38,31 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html.j2", {"request": request})
 
 
-@app.post("/test")
+@app.post("/test/")
 async def test(request: Request, imagen_entrada: Annotated[bytes, File()]):
-    print(imagen_entrada)
+    prediccion = learn.predict(imagen_entrada)
+    predict_id = uuid.uuid4()
+    with open("result.json", "w") as f:
+        f.write(
+            json.dumps(
+                {
+                    "id": str(predict_id),
+                    "prediccion": prediccion[0],
+                    "bytes": base64.b64encode(imagen_entrada).decode("utf-8"),
+                }
+            )
+        )
+    print(prediccion[0])
     return templates.TemplateResponse(
-        "response.html.j2", {"request": request, "name": imagen_entrada}
+        "response.html.j2",
+        {"request": request, "prediccion": prediccion[0], "pred_id": predict_id},
     )
+
+
+@app.get("/feedback")
+def feedback(request: Request, texto_feebdack: str, ident: str):
+    print(ident, texto_feebdack)
+    return "ok"
 
 
 if __name__ == "__main__":
